0cf160372e7bec008ff05ddeee5be9988ed72264
wilberth830 <revwilberth@gmail.com>
2020-10-07 23:20:08 -0500

    actualizado2

---
+++ Menu/menu.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="menustyle1.css">
+</head>
+<body>
+  <header class="class-header">
+    <div class="header-nav contenedor">
+      <div>
+        <a href="../index.php">
+          <img src="img/logo1.png" alt="">
+        </a>
+    </div>
+      <nav>
+        <a href="Comentarios/Comentarios.php">Promociones</a>
+        <a href="./Login/login.php">Login</a>
+      </nav>
+    </div>
+  </header>
+
+<main class="cuerpo-index">
+  <div class="contenedor cuerpo-i2">
+      <h2>Menu</h2>
+      <div class="platillos-index">
+        <div class="card-deck">
+          <div class="card">
+            <img src="img/plato1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Chaufa de Pollo</h5>
+              <p class="card-text">Arroz con un poco de </p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/plato2.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Chicharron de Chancho</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/plato5.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Estofado de Pollo</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div><!--fin cardec-->  
+      </div><!--fin platillos-index-->
+      <div class="platillos-index">
+        <div class="card-deck">
+          <div class="card">
+            <img src="img/plato1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Chaufa de Pollo</h5>
+              <p class="card-text">Arroz con un poco de </p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/plato2.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Chicharron de Chancho</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/plato5.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Estofado de Pollo</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div><!--fin cardec-->  
+      </div><!--fin platillos-index-->
+      <div class="platillos-index">
+        <div class="card-deck">
+          <div class="card">
+            <img src="img/plato1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Chaufa de Pollo</h5>
+              <p class="card-text">Arroz con un poco de </p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/plato2.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Chicharron de Chancho</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/plato5.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Estofado de Pollo</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div><!--fin cardec-->  
+      </div><!--fin platillos-index-->
+  </div><!--fin cuerpo i2-->
+</main>
+  
+
+  <footer class="par-footer">
+    <p>&copy; Casual Food todos los derechos reservados - 2020</p>
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -13,102 +13,14 @@
     <div class="header-nav contenedor">
       <img src="img/logo1.png" alt="">
       <nav>
-        <a href="">Menu</a>
+        <a href="Menu/menu.php">Menu</a>
         <a href="Comentarios/Comentarios.php">Promociones</a>
         <a href="./Login/login.php">Login</a>
       </nav>
     </div>
   </header>
 
-<main class="cuerpo-index">
-  <div class="contenedor cuerpo-i2">
-      <h2>Menu</h2>
-      <div class="platillos-index">
-        <div class="card-deck">
-          <div class="card">
-            <img src="img/plato1.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Chaufa de Pollo</h5>
-              <p class="card-text">Arroz con un poco de </p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-          <div class="card">
-            <img src="img/plato2.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Chicharron de Chancho</h5>
-              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-          <div class="card">
-            <img src="img/plato5.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Estofado de Pollo</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-        </div><!--fin cardec-->  
-      </div><!--fin platillos-index-->
-      <div class="platillos-index">
-        <div class="card-deck">
-          <div class="card">
-            <img src="img/plato1.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Chaufa de Pollo</h5>
-              <p class="card-text">Arroz con un poco de </p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-          <div class="card">
-            <img src="img/plato2.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Chicharron de Chancho</h5>
-              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-          <div class="card">
-            <img src="img/plato5.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Estofado de Pollo</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-        </div><!--fin cardec-->  
-      </div><!--fin platillos-index-->
-      <div class="platillos-index">
-        <div class="card-deck">
-          <div class="card">
-            <img src="img/plato1.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Chaufa de Pollo</h5>
-              <p class="card-text">Arroz con un poco de </p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-          <div class="card">
-            <img src="img/plato2.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Chicharron de Chancho</h5>
-              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-          <div class="card">
-            <img src="img/plato5.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Estofado de Pollo</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-        </div><!--fin cardec-->  
-      </div><!--fin platillos-index-->
-  </div><!--fin cuerpo i2-->
-</main>
+
   
 
   <footer class="par-footer">
