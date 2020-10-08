1ce41c00bc908f309e3eabda1ff84f3e57747eb1
wilberth830 <revwilberth@gmail.com>
2020-10-08 00:13:06 -0500

    agregando index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="indexstyle1.css">
+  <link rel="stylesheet" href="indexstyle.css">
 </head>
 <body>
   <header class="class-header">
@@ -20,7 +20,7 @@
     </div>
   </header>
   <main class="cuerpo-index">
-    
+
   </main>
 
 
